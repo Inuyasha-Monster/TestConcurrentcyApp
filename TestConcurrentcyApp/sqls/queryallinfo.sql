46d8006f41415f2af317fe783043d347a98b3c51
djl <djl@DESKTOP-F5IDQLS>
2016-08-28 00:44:22 +0800

    query

---
+++ TestConcurrentcyApp/sqls/queryallinfo.sql
@@ -0,0 +1,4 @@
+﻿--3. 查询所有同学的学号、姓名、选课数、总成绩；
+select st.ID,st.Name,Count(sc.CourseID),sum(sc.Score) from [dbo].[Student] as st
+left join [dbo].[Score] as sc on st.ID = sc.StudentID
+group by st.ID,st.Name
