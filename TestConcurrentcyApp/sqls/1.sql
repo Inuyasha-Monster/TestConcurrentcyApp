4a79f1bde46c1b54abf812125d0f59739d9a666d
djl <djl@DESKTOP-F5IDQLS>
2016-08-28 00:36:48 +0800

    sql

---
+++ TestConcurrentcyApp/sqls/1.sql
@@ -0,0 +1,3 @@
+﻿select sc.StudentID as 学生编号,st.Name as 学生姓名, avg(sc.Score) as 平均成绩 from Score as sc
+inner join [dbo].[Student] as st on sc.StudentID= st.ID
+group by sc.StudentID,st.Name having AVG(sc.Score)>60
