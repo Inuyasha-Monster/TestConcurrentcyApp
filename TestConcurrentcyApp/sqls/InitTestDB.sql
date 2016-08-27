57b8f643344b2a8509afdd73c2a484a0660370e6
djl <djl@DESKTOP-F5IDQLS>
2016-08-28 00:12:43 +0800

    sql test

---
+++ TestConcurrentcyApp/sqls/InitTestDB.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [Student] (
+  [ID] INTEGER NOT NULL PRIMARY KEY identity, 
+  [Name] NVARCHAR(20), 
+  [Age] INT, 
+  [Sex] INT);
+
+CREATE TABLE [Course] (
+  [ID] INTEGER NOT NULL PRIMARY KEY identity, 
+  [Name] NVARCHAR(20), 
+  [TeacherID] INT) 
+
+CREATE TABLE [Score] (
+  [Score] float,   
+  [StudentID] INT,
+  [CourseID] INT) 
+
+CREATE TABLE [Teacher] (
+  [ID] INTEGER NOT NULL PRIMARY KEY identity,  
+  [Name] NVARCHAR(20)
+)
