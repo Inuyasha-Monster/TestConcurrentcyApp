d729ca8ac5599246819c38c7c53476f2ea9eb599
djl <djl@DESKTOP-F5IDQLS>
2016-08-28 23:00:43 +0800

    proc

---
+++ TestConcurrentcyApp/sqls/proc.sql
@@ -0,0 +1,12 @@
+﻿alter proc optional_parameter
+@name nvarchar(50)=null
+as
+if(@name is not null)
+ select st.Name from dbo.Student as st where st.Name like @name+'%';
+else
+	select st.Name from dbo.Student as  st
+
+exec dbo.optional_parameter
+
+
+
